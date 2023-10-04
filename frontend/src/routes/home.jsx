a92f451ff9360e7b57dbe78cdfae8cd5c4c80c84
mzsellem <marleysellem@gmail.com>
2023-10-04 11:35:34 -0400

    add resume

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,4 +1,7 @@
 import Navbar from "../components/navbar";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 export default function Home() {
    return (
@@ -7,7 +10,13 @@ export default function Home() {
          <div className="flex flex-row w-full justify-between">
             <div className="flex flex-col text-3xl border w-1/2">
                <div className="flex justify-center">Resume</div>
-               <div className="flex justify-center">put pdf here</div>
+               <div className="flex justify-center">
+                  {/* pdfjs uses a web worker to process the most tasks which take
+                  time such as parsing and rendering a PDF document. */}
+                  <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                     <Viewer fileUrl="resume.pdf" />
+                  </Worker>
+               </div>
             </div>
             <div className="flex text-3xl border w-1/2 justify-center">
                Hello BeyondMD!
